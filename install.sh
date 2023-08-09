e592baa42b2e80a1520419b904153b6555dfc56b
TheBlueRuby <109350270+TheBlueRuby@users.noreply.github.com>
2023-08-09 15:53:59 +1000

    using $() instead of while loops

--- install.sh
+++ install.sh
@@ -3,33 +3,21 @@ read "What AUR helper would you like to use? (install first!)" AUR_HELPER
 
 $AUR_HELPER -Syu
 
-cat arch-pkgs.txt | while read pkg
-do
-	$AUR_HELPER -S --needed --noconfirm -- $pkg
-done
+$AUR_HELPER -S --needed --noconfirm -- "$(cat arch-pkgs)"
 
 read "kde, xfce or hyprland (choose one) " de
-cat $de.txt | while read pkg
-do
-	$AUR_HELPER -S --needed --noconfirm -- $pkg
-done
+$AUR_HELPER -S --needed --noconfirm -- "$(cat $de.txt)"
 
 read -p "Install utility software? [y/N] " installUtils
 if [[ "$installUtils" =~ ^([yY][eE][sS]|[yY])$ ]]
 then
-    cat utils.txt | while read pkg
-    do
-    	$AUR_HELPER -S --needed --noconfirm -- $pkg
-    done
+    $AUR_HELPER -S --needed --noconfirm -- "$(cat utils.txt)"
 fi
 
 read -p "Install zsh? [y/N] " installzsh
 if [[ "$installUtils" =~ ^([yY][eE][sS]|[yY])$ ]]
 then
-    cat zsh.txt | while read pkg
-    do
-    	$AUR_HELPER -S --needed --noconfirm -- $pkg
-    done
+    $AUR_HELPER -S --needed --noconfirm -- "$(cat zsh.txt)"
     chsh -s /bin/zsh
 fi
 
